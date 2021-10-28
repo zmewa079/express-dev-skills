866fc3357feb89b6526c5283ba68d19a5a06e1a8
zmewa079 <zannatul.mewa07@gmail.com>
2021-10-28 00:42:25 -0400

    add database

---
+++ data/skill-db.js
@@ -0,0 +1,25 @@
+const skills = [
+  {text: 'Coding Languages', have: true, _id: 123321},
+  {text: 'Database Knowledge', have: true, _id: 543123},
+  {text: 'Data structures and algorithms', have: false, _id: 867543}
+]
+
+const find = (conditions, callback) => {
+  // see if this works, if not, execute the code in the catch block
+  try {
+    // make sure that conditions is an object - if not throw a TypeError
+    if (!(conditions instanceof Object)){
+      throw new TypeError('Please pass in an object')
+    }
+    // If the object is empty, return all the todos
+    if (Object.keys(conditions).length === 0) return callback(null, todos)
+	// deal with errors
+  } catch (error) {
+    console.log(error)
+    callback(error, [])
+  }
+}
+
+export { 
+	find
+}
